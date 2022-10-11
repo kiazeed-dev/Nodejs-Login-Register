7b467720ae9bf7947ec0856286a3c9541765362d
Parunyu Kia <parunyu.kia@gmail.com>
2022-10-11 18:58:47 +0700

    Edited colloection and Add API
    Add API for reset password and list user name.

--- login-example/model/user.js
+++ login-example/model/user.js
@@ -4,8 +4,10 @@ const UserSchema = new mongoose.Schema(
   {
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    fname: {type:String, required:true },
-    lname: {type:String, required:true }
+    fname: { type: String, required: true },
+    lname: { type: String, required: true },
+    question: { type: String, required: true },
+    anwser: { type: String, required: true }
   },
   { collection: "users" }
 );
--- login-example/server.js
+++ login-example/server.js
@@ -21,7 +21,9 @@ app.post("/api/register", async (req, res) => {
     email,
     password: plainTextPassword,
     fname,
-    lname
+    lname,
+    question,
+    anwser: plainTextAnwser,
   } = req.body;
 
   if (!email || typeof email !== "string") {
@@ -40,19 +42,26 @@ app.post("/api/register", async (req, res) => {
   }
 
   const password = await bcrypt.hash(plainTextPassword, 10);
+  const anwser = await bcrypt.hash(plainTextAnwser, 10);
 
   try {
     const response = await User.create({
       email,
       password,
       fname,
-      lname
+      lname,
+      question,
+      anwser,
     });
     console.log("User created successfully: ", response);
   } catch (error) {
+    // duplicate key
     if (error.code === 11000) {
-      // duplicate key
-      return res.json({ status: "error", error: "Email already in use" });
+      return res.json({
+        status: "error",
+        error: "Email already in use",
+        message: error,
+      });
     }
     throw error;
   }
@@ -94,6 +103,91 @@ app.post("/api/authen", (req, res) => {
     res.json({ status: "ok", decoded });
   } catch (err) {
     res.json({ status: "error", message: err.message });
+  }
+});
+
+// forget password
+app.post("/api/forget", async (req, res) => {
+  const { email, question, anwser } = req.body;
+  const user = await User.findOne({ email, question }).lean();
+
+  if (!user) {
+    return res.json({
+      status: "error",
+      error: "Invalid Email/Question/Anwser",
+    });
+  }
+
+  const checkAnwser = await bcrypt.compare(anwser, user.anwser);
+  //if the Email, Question, anwser combination is successful
+  if (checkAnwser) {
+    const token = jwt.sign(
+      {
+        id: user._id,
+        email: user.email,
+      },
+      JWT_SECRET,
+      { expiresIn: "1h" }
+    );
+
+    return res.json({ status: "ok", token: token });
+  }
+
+  res.json({ status: "error", error: "Invalid Email/Question/Anwser" });
+});
+
+// change-password
+app.post("/api/change-password", async (req, res) => {
+  const { token, newpassword: plainTextPassword } = req.body;
+
+  if (!plainTextPassword || typeof plainTextPassword !== "string") {
+    return res.json({ status: "error", error: "Invalid password" });
+  }
+
+  if (plainTextPassword.length < 5) {
+    return res.json({
+      status: "error",
+      error: "Password too small. Should be atleast 6 characters",
+    });
+  }
+
+  try {
+    const user = jwt.verify(token, JWT_SECRET);
+
+    const _id = user.id;
+
+    const password = await bcrypt.hash(plainTextPassword, 10);
+
+    await User.updateOne(
+      { _id },
+      {
+        $set: { password },
+      }
+    );
+    res.json({ status: "ok" });
+  } catch (error) {
+    console.log(error);
+    res.json({ status: "error", error: ";))" });
+  }
+});
+
+app.get("/api/userlist", async (req, res) => {
+  try {
+    await User.aggregate(
+      [
+        {
+          $project: { _id: 1, fname: 1, lname: 1 },
+        },
+      ],
+      (err, result) => {
+        if (err) throw err;
+        // console.log(result);
+        return res.json(result);
+      }
+    );
+  } catch (error) {
+    console.log(error);
+    res.json({ status: "error", error: ";))" });
   }
 });
 
